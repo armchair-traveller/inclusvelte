9d6a6c228db9ba7b7beaccf9460fdb1cc6442ce3
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-03 02:09:48 -0700

    add toggle examples

---
+++ src/lib/switch.svelte
@@ -0,0 +1,14 @@
+<!-- @component
+switches differ from toggles in that they have on/off text labels
+* form els i.e. checkboxes or radio buttons should be used in cases where the switch is not submitting anything
+-->
+<script>
+  let checked = false;
+  let id = "notify-email";
+</script>
+
+<span {id}>Your label name</span>
+<button role="switch" aria-checked={checked} aria-labelledby={id}>
+  <span>on</span>
+  <span>off</span>
+</button>
---
+++ src/lib/toggle.svelte
@@ -0,0 +1,17 @@
+<script>
+  let pressed = false;
+</script>
+
+<button class="{pressed ? 'l' : 'h'} " aria-pressed={pressed} on:click>
+  hi
+</button>
+
+<!-- @component
+  Niche alternative usage:
+  play/pause
+  ```jsx
+  <span aria-hidden="true">{play ? '&#x25b6;' : '&#x23f8;'}</span>
+  <span class="visually-hidden">play</span>
+  ```
+  This component seems so small... there just doesn't seem like any good reason to abstract it.
+ -->
