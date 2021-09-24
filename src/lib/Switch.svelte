51ed01fd6555d1f63e9071a80bb84f80d1f143f9
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-23 19:47:14 -0700

    add switch

--- src/lib/Switch.svelte
+++ src/lib/Switch.svelte
@@ -3,8 +3,8 @@ switches differ from toggles in that they have on/off text labels
 * form els i.e. checkboxes or radio buttons should be used in cases where the switch is not submitting anything
 -->
 <script>
-  let checked = false;
-  let id = "notify-email";
+let checked = false
+let id = 'notify-email'
 </script>
 
 <span {id}>Your label name</span>
