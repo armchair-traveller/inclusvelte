d69f049654bbbc290e457c14fe568f080923961a
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-23 20:18:59 -0700

    move menu button demo to other route

---
+++ src/routes/inclusive-menu-button.svelte
@@ -0,0 +1,34 @@
+<script>
+import useMenu from '$lib/experimental/menu'
+import { fade } from 'svelte/transition'
+const menu = useMenu()
+</script>
+
+{$menu}
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+
+<button use:menu.btn>hiiii</button>
+
+<div class="w-56 text-right fixed top-16">
+  {#if $menu}
+    <menu class="" use:menu transition:fade={{ duration: 200 }}>
+      <button
+        class={`text-gray-900 group flex rounded-md items-center w-full px-2 py-2 text-sm focus:bg-indigo-500`}
+        use:menu.item
+        on:click={menu.toggle}>lhl</button
+      >
+      <button
+        class={`text-gray-900 group flex rounded-md items-center w-full px-2 py-2 text-sm focus:bg-indigo-500`}
+        use:menu.item>lhl</button
+      >
+      <button
+        class={`text-gray-900 group flex rounded-md items-center w-full px-2 py-2 text-sm focus:bg-indigo-500`}
+        use:menu.item>lhl</button
+      >
+    </menu>
+  {/if}
+</div>
