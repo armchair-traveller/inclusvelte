95c939a1db2b3f74b598737461309c15ce30ef66
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-09-23 19:46:24 -0700

    experimental menu button

---
+++ src/lib/menu-button.svelte
@@ -0,0 +1,28 @@
+<!--
+  adapts https://github.com/KittyGiraudel/react-menu-button
+-->
+<script>
+  import InclusiveMenuButton from "inclusive-menu-button";
+  import { onMount } from "svelte";
+  let buttonEl,
+    id,
+    label,
+    menu = null,
+    options;
+
+  onMount(() => {
+    // need a way to either pass this to parent, or... expose prop like open which directs this reactively $:
+    menu = new InclusiveMenuButton(buttonEl, options);
+  });
+</script>
+
+<menu>
+  <button bind:this={buttonEl} type="button" data-inclusive-menu-opens={id}>
+    {label}
+    <span aria-hidden="true">▾</span>
+  </button>
+
+  <div {id}>
+    <slot />
+  </div>
+</menu>
